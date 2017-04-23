051cc6b1c51ca842f5c9fbdfe67ac4734069547c
dreamcatcher1 <1176670601@qq.com>
2017-04-23 22:12:36 +0800

    二次提交

--- app/src/main/java/activitytest/example/com/activitytest/FirstActivity.java
+++ app/src/main/java/activitytest/example/com/activitytest/FirstActivity.java
@@ -1,7 +1,10 @@
 package activitytest.example.com.activitytest;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,14 +17,25 @@ public class FirstActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+        Log.d("FirstActivity","ffffffggg");
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(FirstActivity.this,"You clicked Button1",Toast.LENGTH_SHORT).show();
+                //toast test
+//                Toast.makeText(FirstActivity.this,"You clicked Button1",Toast.LENGTH_SHORT).show();
+                //显式intent test
+//                Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
+                //隐式intent test
+/*                Intent intent = new Intent("activitytest.example.com.activitytest.ACTION_START");
+                intent.addCategory("com.example.activitytest.MY_CATEGORY");*/
+                Log.d("FirstActivity","adfdfggg");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
+
+                startActivity(intent);
             }
         });
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
